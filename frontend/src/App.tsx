afba97e10052a9a4c94b3852149d0bacb32f44dd
Kainoa Gardner <kainoagardner123@gmail.com>
2025-07-17 13:20:21 -0700

    start frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,29 @@
+// import Login from "./login.jsx";
+import { useState } from "react"
+
 function App() {
+  const [page, setPage] = useState("home")
 
   return (
     <>
-      <div>
+      <div className="nav">
+        <button
+          className={page === "home" ? "on navButton" : "navButton"}
+          onClick={() => setPage("home")}>
+          Home </button>
+        <button
+          className={page === "login" ? "on navButton" : "navButton"}
+          onClick={() => setPage("login")}>
+          Login </button>
+
       </div>
+      {/* <Tab page={page} /> */}
     </>
   )
 }
+
+// function Tab(page) {
+//
+// }
 
 export default App
---
+++ frontend/src/login.tsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <>
+      <h1>Login</h1>
+      <hr />
+    </>
+  );
+}
+export default Login;
