c4357069c8fefd8316aea73a68063233c174c30b
Kainoa Gardner <kainoagardner123@gmail.com>
2025-08-06 21:17:13 -0700

    finished engine movement checks

--- frontend/src/game/engine.ts
+++ frontend/src/game/engine.ts
@@ -7,13 +7,24 @@ export function getMoveDirection(owner: number): number {
   else { return -1 }
 }
 
+function getEnemyTurnInt(game: Game): number {
+  if (game.turn === 0) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
 export function checkValidPieceMove(start: Vec2, end: Vec2, piece: Piece, game: Game): boolean {
-  const dir = getMoveDirection(game.userSide)
+  const dir = getMoveDirection(game.turn)
   const possibleMoves = getPieceMoves(start, piece, game, dir)
   const filteredMoves = filterPossibleMoves(start, possibleMoves, game)
+  console.log("Possible", possibleMoves)
+  console.log("Filtered", filteredMoves)
 
   return checkEndPosInPossibleMoves(end, filteredMoves)
 }
+
 
 export function getPieceMoves(start: Vec2, piece: Piece, game: Game, dir: number): Vec2[] {
   let possibleMoves: Vec2[] = []
@@ -114,12 +125,39 @@ export function getPieceMoves(start: Vec2, piece: Piece, game: Game, dir: number
 }
 
 export function filterPossibleMoves(start: Vec2, possibleMoves: Vec2[], game: Game): Vec2[] {
-  return []
+  for (let i = possibleMoves.length - 1; i >= 0; i--) {
+    const movePos = possibleMoves[i]
+    const gameCopy: Game = copyGame(game)
+    const piece = gameCopy.board[start.y][start.x]
+
+    if (piece !== null && piece.type >= PieceEnum.Pawn && piece.type <= PieceEnum.Ryuu) {
+      gameCopy.board[start.y][start.x] = null
+      gameCopy.board[movePos.y][movePos.x] = piece
+      if (getInCheck(gameCopy)) {
+        possibleMoves.splice(i, 1)
+      }
+    } else if (piece !== null && piece.type >= PieceEnum.Checker && piece.type <= PieceEnum.CheckerKing) {
+      if (checkerMovesInCheck(start, movePos, piece, gameCopy)) {
+        possibleMoves.splice(i, 1)
+      }
+    }
+  }
+
+  return possibleMoves
 }
 
 export function checkEndPosInPossibleMoves(end: Vec2, possibleMoves: Vec2[]): boolean {
+  for (let i = 0; i < possibleMoves.length; i++) {
+    const possibleMove = possibleMoves[i]
+
+    if (checkVec2Equal(end, possibleMove)) {
+      return true
+    }
+  }
+
   return false
 }
+
 
 function checkPositionInbounds(pos: Vec2, game: Game): boolean {
   if (pos.x < 0 || pos.x >= game.width) {
@@ -159,7 +197,7 @@ function getPawnMoves(pos: Vec2, piece: Piece, game: Game, direction: number): V
   //capture squares
   const relativeMovePos = [{ x: -1, y: -1 }, { x: 1, y: -1 }]
   for (let i = 0; i < relativeMovePos.length; i++) {
-    const newPos = relativeMovePos[i]
+    const newPos = { x: relativeMovePos[i].x, y: relativeMovePos[i].y }
     newPos.y *= direction
 
     newPos.x += pos.x
@@ -193,7 +231,7 @@ function getKnightMoves(pos: Vec2, piece: Piece, game: Game): Vec2[] {
 
 
   for (let i = 0; i < relativeMovePos.length; i++) {
-    const newPos = relativeMovePos[i]
+    const newPos = { x: relativeMovePos[i].x, y: relativeMovePos[i].y }
     newPos.x += pos.x
     newPos.y += pos.y
 
@@ -224,7 +262,7 @@ function getBishopMoves(pos: Vec2, piece: Piece, game: Game): Vec2[] {
     let j = 0
     while (j >= 0) {
       j++
-      const newPos = pos
+      const newPos = { x: pos.x, y: pos.y }
 
       newPos.x += dir.x * j
       newPos.y += dir.y * j
@@ -264,7 +302,7 @@ function getRookMoves(pos: Vec2, piece: Piece, game: Game): Vec2[] {
     let j = 0
     while (j >= 0) {
       j++
-      const newPos = pos
+      const newPos = { x: pos.x, y: pos.y }
 
       newPos.x += dir.x * j
       newPos.y += dir.y * j
@@ -316,7 +354,7 @@ function getKingMoves(pos: Vec2, piece: Piece, game: Game): Vec2[] {
   for (let i = 0; i < directions.length; i++) {
     const dir = directions[i]
 
-    const newPos = pos
+    const newPos = { x: pos.x, y: pos.y }
     newPos.x += dir.x
     newPos.y += dir.y
 
@@ -361,7 +399,7 @@ function getCastleMoves(pos: Vec2, piece: Piece, game: Game): Vec2[] {
 function getFuMoves(pos: Vec2, piece: Piece, game: Game, direction: number): Vec2[] {
   const result: Vec2[] = []
 
-  const newPos = pos
+  const newPos = { x: pos.x, y: pos.y }
   newPos.y += -1 * direction
 
   if (checkPositionInbounds(newPos, game)) {
@@ -380,7 +418,7 @@ function getKyouMoves(pos: Vec2, piece: Piece, game: Game, dir: number): Vec2[] 
   let i = 0
   while (i >= 0) {
     i++
-    const newPos = pos
+    const newPos = { x: pos.x, y: pos.y }
 
     newPos.y += -i * dir
 
@@ -413,7 +451,7 @@ function getKeiMoves(pos: Vec2, piece: Piece, game: Game, direction: number): Ve
   for (let i = 0; i < directions.length; i++) {
     const dir = directions[i]
 
-    const newPos = pos
+    const newPos = { x: pos.x, y: pos.y }
     newPos.x += dir.x
     newPos.y += dir.y
 
@@ -442,7 +480,7 @@ function getGinMoves(pos: Vec2, piece: Piece, game: Game, dir: number): Vec2[] {
   for (let i = 0; i < directions.length; i++) {
     const dir = directions[i]
 
-    const newPos = pos
+    const newPos = { x: pos.x, y: pos.y }
     newPos.x += dir.x
     newPos.y += dir.y
 
@@ -472,7 +510,7 @@ function getKinMoves(pos: Vec2, piece: Piece, game: Game, dir: number): Vec2[] {
   for (let i = 0; i < directions.length; i++) {
     const dir = directions[i]
 
-    const newPos = pos
+    const newPos = { x: pos.x, y: pos.y }
     newPos.x += dir.x
     newPos.y += dir.y
 
@@ -500,7 +538,7 @@ function getUmaMoves(pos: Vec2, piece: Piece, game: Game): Vec2[] {
   for (let i = 0; i < directions.length; i++) {
     const dir = directions[i]
 
-    const newPos = pos
+    const newPos = { x: pos.x, y: pos.y }
     newPos.x += dir.x
     newPos.y += dir.y
 
@@ -530,7 +568,7 @@ function getRyuuMoves(pos: Vec2, piece: Piece, game: Game): Vec2[] {
   for (let i = 0; i < directions.length; i++) {
     const dir = directions[i]
 
-    const newPos = pos
+    const newPos = { x: pos.x, y: pos.y }
     newPos.x += dir.x
     newPos.y += dir.y
 
@@ -558,8 +596,8 @@ function getCheckerMoves(pos: Vec2, piece: Piece, game: Game, dir: number): Vec2
   for (let i = 0; i < directions.length; i++) {
     const dir = directions[i]
 
-    const jumpPos = pos
-    const landPos = pos
+    const jumpPos = { x: pos.x, y: pos.y }
+    const landPos = { x: pos.x, y: pos.y }
 
     jumpPos.x += dir.x
     jumpPos.y += dir.y
@@ -598,8 +636,8 @@ function getCheckerKingMoves(pos: Vec2, piece: Piece, game: Game): Vec2[] {
 
   for (let i = 0; i < directions.length; i++) {
     const dir = directions[i]
-    const jumpPos = pos
-    const landPos = pos
+    const jumpPos = { x: pos.x, y: pos.y }
+    const landPos = { x: pos.x, y: pos.y }
 
     jumpPos.x += dir.x
     jumpPos.y += dir.y
@@ -623,4 +661,191 @@ function getCheckerKingMoves(pos: Vec2, piece: Piece, game: Game): Vec2[] {
   }
 
   return result
+}
+
+
+function copyGame(game: Game): Game {
+  const gameCopy = new Game(game.id, game.width, game.height, game.placeLine, game.userSide, game.money, game.time)
+
+  const boardCopy = Array.from({ length: gameCopy.height }, () => Array(gameCopy.width).fill(null))
+
+  for (let i = 0; i < game.height; i++) {
+    for (let j = 0; j < game.width; j++) {
+      const piece = game.board[i][j]
+      if (piece !== null) {
+        const pieceCopy = new Piece(piece.x, piece.y, piece.type, piece.owner, piece.moved)
+        boardCopy[i][j] = pieceCopy
+      } else {
+        boardCopy[i][j] = null
+      }
+    }
+  }
+
+  gameCopy.board = boardCopy
+
+  if (game.enPassant !== null) {
+    const enPassantPos = { x: game.enPassant.x, y: game.enPassant.y }
+    gameCopy.enPassant = enPassantPos
+  } else {
+    gameCopy.enPassant = null
+  }
+
+  if (game.checkerJump !== null) {
+    const checkerJump = { x: game.checkerJump.x, y: game.checkerJump.y }
+    gameCopy.checkerJump = checkerJump
+  } else {
+    gameCopy.checkerJump = null
+  }
+
+  return gameCopy
+}
+
+
+function getInCheck(game: Game): boolean {
+  const kings = getKingPos(game)
+  for (let i = 0; i < kings.length; i++) {
+    const king = kings[i]
+
+    if (checkUnderAttack(king, game)) {
+      return true
+    }
+  }
+
+  return false
+}
+
+function getKingPos(game: Game): Vec2[] {
+  const result: Vec2[] = []
+
+  for (let i = 0; i < game.height; i++) {
+    for (let j = 0; j < game.width; j++) {
+      const space = game.board[i][j]
+
+      if (space !== null && space.owner === game.turn && (space.type === PieceEnum.King || space.type === PieceEnum.Ou)) {
+        console.log({ x: j, y: i })
+        result.push({ x: j, y: i })
+      }
+    }
+  }
+
+  return result
+}
+
+function checkUnderAttack(pos: Vec2, game: Game): boolean {
+  const attackSpace: Map<string, boolean> = new Map<string, boolean>()
+
+  game.turn = getEnemyTurnInt(game)
+  for (let i = 0; i < game.height; i++) {
+    for (let j = 0; j < game.width; j++) {
+      const space = game.board[i][j]
+
+      if (space !== null && space.owner === game.turn) {
+        const pos = { x: j, y: i }
+        const dir = getMoveDirection(game.turn)
+        const possibleMoves = getPieceMoves(pos, space, game, dir)
+        for (let k = 0; k < possibleMoves.length; k++) {
+          const move = possibleMoves[k]
+          const moveString = `${move.x},${move.y}`
+          attackSpace.set(moveString, true)
+        }
+      }
+    }
+
+  }
+
+
+  const posString = `${pos.x},${pos.y}`
+  const underAttack = attackSpace.get(posString)
+  if (underAttack === undefined) {
+    return false
+  } else {
+    return underAttack
+  }
+}
+
+function checkerMovesInCheck(startPos: Vec2, endPos: Vec2, piece: Piece, game: Game): boolean {
+  if (!checkCheckerNextJumps(startPos, endPos, piece, game)) {
+    game.board[startPos.y][startPos.x] = null
+    game.board[endPos.y][endPos.x] = piece
+    if (getInCheck(game)) {
+      return true
+    } else {
+      return false
+    }
+  } else {
+
+    const dir = getMoveDirection(game.turn)
+    let possibleMoves: Vec2[] = []
+    switch (piece.type) {
+      case PieceEnum.Checker: {
+        possibleMoves = getCheckerMoves(endPos, piece, game, dir)
+        break
+      }
+      case PieceEnum.CheckerKing: {
+        possibleMoves = getCheckerKingMoves(endPos, piece, game)
+        break
+      }
+    }
+    for (let i = possibleMoves.length - 1; i >= 0; i--) {
+      if (!checkCheckerTake(endPos, possibleMoves[i])) {
+        possibleMoves.splice(i, 1)
+      }
+    }
+
+    for (let i = 0; i < possibleMoves.length; i++) {
+      const movePos = { x: possibleMoves[i].x, y: possibleMoves[i].y }
+      const gameCopy = copyGame(game)
+      gameCopy.board[startPos.y][startPos.x] = null
+      gameCopy.board[endPos.y][endPos.x] = piece
+
+      const result = checkerMovesInCheck(endPos, movePos, piece, gameCopy)
+
+      if (!result) {
+        return false
+      }
+    }
+  }
+
+  return true
+}
+
+function checkCheckerNextJumps(startPos: Vec2, endPos: Vec2, piece: Piece, game: Game): boolean {
+  if (!checkCheckerTake(startPos, endPos)) {
+    return false
+  }
+
+  const dir = getMoveDirection(game.turn)
+  let possibleMoves: Vec2[] = []
+
+  switch (piece.type) {
+    case PieceEnum.Checker: {
+      possibleMoves = getCheckerMoves(endPos, piece, game, dir)
+      break
+    }
+    case PieceEnum.CheckerKing: {
+      possibleMoves = getCheckerKingMoves(endPos, piece, game)
+      break
+    }
+    default:
+      return false
+  }
+
+  for (let i = possibleMoves.length - 1; i >= 0; i--) {
+    if (!checkCheckerTake(endPos, possibleMoves[i])) {
+      possibleMoves.splice(i, 1)
+    }
+  }
+
+  return possibleMoves.length > 0
+}
+
+function checkCheckerTake(startPos: Vec2, endPos: Vec2): boolean {
+  const dx = Math.abs(startPos.x - endPos.x)
+  const dy = Math.abs(startPos.y - endPos.y)
+
+  if (dx === 2 && dy === 2) {
+    return true
+  }
+
+  return false
 }
